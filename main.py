d197003dac84477e529fec106a881feec7c3c8ec
trGiang99 <37588363+trGiang99@users.noreply.github.com>
2020-08-05 21:20:41 +0700

    update "main.py" call to kNN

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import os
 from DataLoader import DataLoader
+
+from neighborhoodBased import kNN
 
 manipulator = DataLoader("movielens-sample")
 
 manipulator.read_data()
 
-train_data, test_data = manipulator.split_data(test_ratio=0.2)
+manipulator.split_data(test_ratio=0.2)
 
-print (f"Training Data:\nLenght: {len(train_data)}")
-print(train_data.head(5))
+train_data, test_data = manipulator.load_data()
 
-print (f"Test Data:\nLenght: {len(test_data)}")
-print(test_data.head(5))
+
+# print (f"Training Data:\n{train_data}")
+# print (f"Test Data:\n{test_data}")
+
+
+knn = kNN(data=train_data, k=10, distance="cosine", uuCF=0)
+
+print(knn.prediction(1, 11))
