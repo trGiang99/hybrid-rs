da616669a890ee75116b4536e91395212ab7f3b3
trGiang99 <37588363+trGiang99@users.noreply.github.com>
2020-08-06 20:47:20 +0700

    update "load" method

--- DataLoader.py
+++ DataLoader.py
@@ -8,18 +8,19 @@ from scipy import sparse
 class DataLoader:
     """
     Contain methods to read and split dataset into training set and test set.
-    """
-    def __init__(self, data_folder):
-        """
-        Args:
+
+    Args:
             data_folder (string): Path to folder contain dataset
-        """
+            test_ratio (float, optional): Size ratio of test set to dataset. Defaults to 0.1.
+    """
+    def __init__(self, data_folder, test_ratio=0.1):
         self.__data_folder = data_folder
+        self.__test_ratio = test_ratio
         self.__ratings = []
         self.__train_data = []
         self.__test_data = []
 
-    def read_data(self):
+    def __read_data(self):
         """ Read ratings from data_folder
         """
         names = ['user_id', 'item_id', 'rating', 'timestamp']
@@ -28,15 +29,12 @@ class DataLoader:
             header=0, names=names
         )
 
-    def split_data(self, test_ratio=0.1):
+    def __split_data(self):
         """Randomly split dataset into training set and test set.
-
-        Args:
-            test_ratio (float, optional): Size ratio of test set to dataset. Defaults to 0.1.
         """
         mask = [
             True if x == 1 else False
-                for x in np.random.uniform(0, 1, (len(self.__ratings))) < 1 - test_ratio
+                for x in np.random.uniform(0, 1, (len(self.__ratings))) < 1 - self.__test_ratio
         ]
 
         # Keep the last user and item on the training set
@@ -47,14 +45,20 @@ class DataLoader:
         neg_mask = [not x for x in mask]
         self.__train_data, self.__test_data = self.__ratings[mask], self.__ratings[neg_mask]
 
-    def load_data(self):
+    def load(self):
         """Convert dataframe of training set to scipy.sparse matrix
         Row u is the ratings that user u has given to all item.
         Column i is the ratings that all users have given to item i.
         """
+        self.__read_data()
+        self.__split_data()
+
         train_data = sparse.csr_matrix((
             self.__train_data["rating"],
             (self.__train_data["user_id"], self.__train_data["item_id"])
         ))
+
+        # Reset the index of test_data dataframe to 0
+        self.__test_data.reset_index(drop=True, inplace=True)
 
         return train_data, self.__test_data
