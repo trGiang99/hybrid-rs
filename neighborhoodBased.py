7a01002d43a420a644949ac081067ea08b4e2668
trGiang99 <37588363+trGiang99@users.noreply.github.com>
2020-08-08 22:59:11 +0700

    Use similarity matrix to speed up process

--- neighborhoodBased.py
+++ neighborhoodBased.py
@@ -28,7 +28,22 @@ class kNN:
         self.baseline = baseline
         self.uuCF = uuCF
 
-    def prediction(self, u, i):
+        if(uuCF):
+            self.S = np.zeros([self.ultility.shape[0], self.ultility.shape[0]])
+        else:
+            self.S = np.zeros([self.ultility.shape[1], self.ultility.shape[1]])
+
+    def fit(self):
+        """Calculate the similarity matrix
+        """
+        self.distance()
+
+        # Turn the triangle matrix self.S into a full matrix
+        full_S = self.S.T + self.S
+        np.fill_diagonal(full_S, np.diag(self.S))
+        self.S = full_S
+
+    def predict(self, u, i):
         """Predict the rating of user u for item i
 
         Args:
@@ -44,31 +59,31 @@ class kNN:
             # Find users that have rated item i beside user u
             user_rated_i = self.ultility.getcol(i).nonzero()[0]
 
-            sim = dict(zip(user_rated_i, [self.distance(u, v) for v in user_rated_i]))
+            sim = self.S[u, user_rated_i]
 
             # Sort similarity list in descending
-            sim = {k: v for k, v in sorted(sim.items(), reverse=True, key=lambda item: item[1])}
+            k_nearest_users = np.argsort(sim)[-self.k:]
 
-            # Get first k users or all if number of users smaller than k
-            user_rated_i = [*sim.keys()][:self.k]
-            sim = np.array([*sim.values()])[:self.k]
+            # Get first k users or all if number of similar users smaller than k
+            sim = sim[k_nearest_users]
+            ratings = np.array([self.ultility[v, i] for v in user_rated_i[k_nearest_users]])
 
-            prediction = np.sum(np.multiply(np.array([self.ultility[v,i] for v in user_rated_i]), sim)) / (np.sum(sim) + 1e-8)
+            prediction = np.sum(sim * ratings) / (np.sum(sim) + 1e-8)
         # Item based CF
         else:
             # Find items that have been rated by user u beside item i
             items_ratedby_u = self.ultility.getrow(u).nonzero()[1]
 
-            sim = dict(zip(items_ratedby_u, [self.distance(i, j) for j in items_ratedby_u]))
+            sim = self.S[i, items_ratedby_u]
 
-            # Sort similarity list in descending order
-            sim = {k: v for k, v in sorted(sim.items(), reverse=True, key=lambda item: item[1])}
+            # Sort similarity list in descending
+            k_nearest_items = np.argsort(sim)[-self.k:]
 
-            # Get first k users or all if number of users smaller than k
-            items_ratedby_u = [*sim.keys()][:self.k]
-            sim = np.array([*sim.values()])[:self.k]
+            # Get first k items or all if number of similar items smaller than k
+            sim = sim[k_nearest_items]
+            ratings = np.array([self.ultility[u, j] for j in user_rated_i[k_nearest_items]])
 
-            prediction = np.sum(np.multiply(np.array([self.ultility[u,j] for j in items_ratedby_u]), sim)) / (np.sum(sim) + 1e-8)
+            prediction = np.sum(sim * ratings) / (np.sum(sim) + 1e-8)
 
         return prediction
 
@@ -86,7 +101,26 @@ class kNN:
         """
         pass
 
-    def __cosine(self, i, j):
+    def rmse(self, test_data):
+        """Calculate Root Mean Squared Error on the test data
+
+        Args:
+            test_data (DataFrame): testing data
+
+        Returns:
+            float: RMSE
+        """
+        squared_error = 0
+        n_test_ratings = test_data.shape[0]
+
+        for n in range(n_test_ratings):
+            # print(f"Predicting {test_data['user_id'][n]},{test_data['item_id'][n]}")
+            pred = self.predict(test_data["user_id"][n], test_data["item_id"][n])
+            squared_error += (pred - test_data["rating"][n])**2
+
+        return np.sqrt(squared_error/n_test_ratings)
+
+    def __cosine(self):
         """Calculate cosine simularity score between object i and object j.
         Object i and j could be item or user, but must be the same.
 
@@ -99,24 +133,31 @@ class kNN:
         """
         # User based CF
         if(self.uuCF):
-            sum_ratings = self.ultility[i,:] * self.ultility[j,:].transpose()
-            if (not sum_ratings):
-                return 0
+            n_users = self.ultility.shape[0] - 1
+            for i in range(1, n_users + 1):
+                for j in range(i, n_users + 1):
+                    sum_ratings = self.ultility[i,:] * self.ultility[j,:].transpose()
+                    if (not sum_ratings):
+                        self.S[i][j] = 0
+                        continue
 
-            norm2_ratings_i = norm(self.ultility[i,:], 'fro')
-            norm2_ratings_j = norm(self.ultility[j,:], 'fro')
+                    norm2_ratings_i = norm(self.ultility[i,:], 'fro')
+                    norm2_ratings_j = norm(self.ultility[j,:], 'fro')
 
-            return (sum_ratings / (norm2_ratings_i * norm2_ratings_j)).data[0]
+                    self.S[i][j] = (sum_ratings / (norm2_ratings_i * norm2_ratings_j)).data[0]
         # Item based CF
         else:
-            sum_ratings = self.ultility[:,i].transpose() * self.ultility[:,j]
-            if (not sum_ratings):
-                return 0
+            n_items = self.ultility.shape[1] - 1
+            for i in range(1, n_items + 1):
+                for j in range(i, n_items + 1):
+                    sum_ratings = self.ultility[:,i].transpose() * self.ultility[:,j]
+                    if (not sum_ratings):
+                        return 0
 
-            norm2_ratings_i = norm(self.ultility[:,i], 'fro')
-            norm2_ratings_j = norm(self.ultility[:,j], 'fro')
+                    norm2_ratings_i = norm(self.ultility[:,i], 'fro')
+                    norm2_ratings_j = norm(self.ultility[:,j], 'fro')
 
-            return (sum_ratings / (norm2_ratings_i * norm2_ratings_j)).data[0]
+                    self.S[i][j] = (sum_ratings / (norm2_ratings_i * norm2_ratings_j)).data[0]
 
     def __pcc(self, i, j):
         pass
