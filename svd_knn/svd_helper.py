cad80d7aac82269c9087767ff505d4346888b498
trGiang99 <37588363+trGiang99@users.noreply.github.com>
2020-09-19 13:52:18 +0700

    Split hybrid's helper func from svd's helper

--- svd_knn/svd_helper.py
+++ svd_knn/svd_helper.py
@@ -7,9 +7,8 @@ def _shuffle(X):
     np.random.shuffle(X)
     return X
 
-
 @njit
-def _run_epoch(X, pu, qi, bu, bi, S, k, global_mean, n_factors, lr_pu, lr_qi, lr_bu, lr_bi, reg_pu, reg_qi, reg_bu, reg_bi):
+def _run_epoch(X, pu, qi, bu, bi, global_mean, n_factors, lr_pu, lr_qi, lr_bu, lr_bi, reg_pu, reg_qi, reg_bu, reg_bi):
     """Runs an epoch, updating model weights (pu, qi, bu, bi).
     Args:
         X (numpy array): training set.
@@ -17,8 +16,6 @@ def _run_epoch(X, pu, qi, bu, bi, S, k, global_mean, n_factors, lr_pu, lr_qi, lr
         qi (numpy array): items latent factor matrix.
         bu (numpy array): users biases vector.
         bi (numpy array): items biases vector.
-        S (numpy array): similarity matrix.
-        k: (numpy array): k nearest neighbors.
         global_mean (float): ratings arithmetic mean.
         n_factors (int): number of latent factors.
         lr (float): learning rate.
@@ -40,31 +37,6 @@ def _run_epoch(X, pu, qi, bu, bi, S, k, global_mean, n_factors, lr_pu, lr_qi, lr
         for factor in range(n_factors):
             pred += pu[user, factor] * qi[item, factor]
 
-        # Find items that have been rated by user u beside item i
-        items_ratedby_u = np.array([
-            int(i_id) for i_id, u_id in zip(X[:,1], X[:,0])
-                if i_id != item and u_id == user
-        ])
-
-        sim = []
-        for item_ratedby_u in items_ratedby_u:
-            sim.append(S[item, item_ratedby_u]) if S[item, item_ratedby_u] else sim.append(S[item_ratedby_u, item])
-
-        sim = np.array(sim)
-
-        # Sort similarity list in descending and get k first indices
-        k_nearest_items = np.argsort(sim)[-k:]
-
-        # Get first k items or all if number of similar items smaller than k
-        sim = sim[k_nearest_items]
-        sim_ratings = np.array([
-            X[np.where((X[:,1] == j) & (X[:,0] == user))[0][0], 2] for j in items_ratedby_u[k_nearest_items]
-        ])
-
-        sim_ratings -= pred
-
-        pred += np.sum(sim * sim_ratings) / (np.abs(sim).sum() + 1e-8)
-
         err = rating - pred
         residuals.append(err)
 
@@ -83,3 +55,43 @@ def _run_epoch(X, pu, qi, bu, bi, S, k, global_mean, n_factors, lr_pu, lr_qi, lr
     residuals = np.array(residuals)
     train_loss = np.square(residuals).mean()
     return pu, qi, bu, bi, train_loss
+
+
+@njit
+def _compute_val_metrics(X_val, pu, qi, bu, bi, global_mean, n_factors):
+    """Computes validation metrics (loss, rmse, and mae).
+    Args:
+        X_val (numpy array): validation set.
+        pu (numpy array): users latent factor matrix.
+        qi (numpy array): items latent factor matrix.
+        bu (numpy array): users biases vector.
+        bi (numpy array): items biases vector.
+        global_mean (float): ratings arithmetic mean.
+        n_factors (int): number of latent factors.
+    Returns:
+        (tuple of floats): validation loss, rmse and mae.
+    """
+    residuals = []
+
+    for i in range(X_val.shape[0]):
+        user, item, rating = int(X_val[i, 0]), int(X_val[i, 1]), X_val[i, 2]
+        pred = global_mean
+
+        if user > -1:
+            pred += bu[user]
+
+        if item > -1:
+            pred += bi[item]
+
+        if (user > -1) and (item > -1):
+            for factor in range(n_factors):
+                pred += pu[user, factor] * qi[item, factor]
+
+        residuals.append(rating - pred)
+
+    residuals = np.array(residuals)
+    loss = np.square(residuals).mean()
+    rmse = np.sqrt(loss)
+    mae = np.absolute(residuals).mean()
+
+    return loss, rmse, mae
